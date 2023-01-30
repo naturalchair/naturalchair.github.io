34e5a485b893feb71a05def16649eaa818de852d
naturalchair <105501523+naturalchair@users.noreply.github.com>
2023-01-30 12:18:00 -0500

    Delete src directory

--- src/firstPage.ts
+++
@@ -1,7 +0,0 @@
-const difficultySelections = Array.from(document.querySelectorAll<HTMLButtonElement>(".cards__button")) 
-
-difficultySelections.forEach(choice => {
-    choice.addEventListener("click", () => {     
-        window.location.assign("questions.html?difficulty=" + choice!.value)
-    })
-})
